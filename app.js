a601a558b0b92571bb5d43f88b39bd8bfef305be
Omiso <misasuzuki1027@gmail.com>
2023-10-28 23:59:40 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 'use strict';
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
+  }
+  return fib(n-1) + fib(n-2);
+}
+
+/**
+ * n === 2
+ * return fib(2-1) + fib(2-2);
+ * return fib(1)+fib(0);
+ * return 1+0;
+ * return 1;
+ * 
+ * n === 3
+ * return fib(3-1) + fib(3-2);
+ * return fib(2)+fib(1);
+ * return fib(1)+fib(0) +fib(1);
+ * return 1 + 0 + 1;
+ * return 2;
+ * 
+ */
+
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
+}
